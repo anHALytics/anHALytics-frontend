69d7c7730ec6b87f256412804b4dce970ae3597d
Aazhar <achraf.azhar@inria.fr>
2016-07-25 10:25:02 +0200

    Revert close button of nerd disamb panel.

--- js/disambiguation.js
+++ js/disambiguation.js
@@ -10,59 +10,55 @@ var parseDisambNERD = function (sdata) {
 };
 
 
-var showexpandpre = function (sdata) {
-    showexpand(decodeURIComponent(sdata['content']));
-};
+//var showexpandpre = function (sdata) {
+//    showexpand(decodeURIComponent(sdata['content']));
+//};
+//
+//var showexpand = function (sdata) {
+//    if (!sdata) {
+//        return;
+//    }
+//    if (sdata.indexOf("----------") == -1) {
+//        return;
+//    }
+//
+//    var jsonObject = parseDisamb(sdata);
+//    //console.log(jsonObject);
+//
+//    $('#disambiguation_panel').empty();
+//    var piece = '<div class="well" > \
+//				   		 <div class="row"><div class="col-md-12">';
+//    if (jsonObject['senses']) {
+//        piece += '<table class="table" style="width:100%;border:1px solid white;">';
+//        for (var sens in jsonObject['senses']) {
+//
+//            piece += '<tr><td><strong>' + jsonObject['senses'][sens]['label'] + '&nbsp;</strong></td><td>' +
+//                    jsonObject['senses'][sens]['desc']
+//                    + '</td><td>';
+//            if (jsonObject['senses'][sens]['wiki']) {
+//                piece += '<a href="http://en.wikipedia.org/wiki?curid=' +
+//                        jsonObject['senses'][sens]['wiki'] +
+//                        '" target="_blank"><img style="max-width:28px;max-height:22px;" src="data/images/wikipedia.png"/></a>';
+//            }
+//            piece += '</td></tr>';
+//        }
+//        piece += '</table>';
+//    }
+//
+//    for (var surf in jsonObject['paraphrases']) {
+//        piece += '<p>' + jsonObject['paraphrases'][surf] + '</p>';
+//    }
+//
+//
+//    piece += '<div class="col-md-1"><a id="close-disambiguate-panel" style="display:none" onclick=\'$("#disambiguation_panel").hide()\'><span class="glyphicon glyphicon-remove"></span></a></div>';
+//    $('#disambiguation_panel').append(piece);
+//
+//    $('#disambiguation_panel').show();
+//};
 
-var showexpand = function (sdata) {
-    if (!sdata) {
-        return;
-    }
-    if (sdata.indexOf("----------") == -1) {
-        return;
-    }
+var getPieceShowexpandNERD = function (jsonObject) {
 
-    var jsonObject = parseDisamb(sdata);
-    //console.log(jsonObject);
-
-    $('#disambiguation_panel').empty();
-    var piece = '<div class="well" style="background-color:#F7EDDC;"> \
-				   		 <div class="row"><div class="col-md-12">';
-    if (jsonObject['senses']) {
-        piece += '<table class="table" style="width:100%;border:1px solid white;">';
-        for (var sens in jsonObject['senses']) {
-
-            piece += '<tr><td><strong>' + jsonObject['senses'][sens]['label'] + '&nbsp;</strong></td><td>' +
-                    jsonObject['senses'][sens]['desc']
-                    + '</td><td>';
-            if (jsonObject['senses'][sens]['wiki']) {
-                piece += '<a href="http://en.wikipedia.org/wiki?curid=' +
-                        jsonObject['senses'][sens]['wiki'] +
-                        '" target="_blank"><img style="max-width:28px;max-height:22px;" src="data/images/wikipedia.png"/></a>';
-            }
-            piece += '</td></tr>';
-        }
-        piece += '</table>';
-    }
-
-    for (var surf in jsonObject['paraphrases']) {
-        piece += '<p>' + jsonObject['paraphrases'][surf] + '</p>';
-    }
-
-
-    piece += '</div><div class="col-md-1"><div id="close-disambiguate-panel" \
-					  style="position:relative;float:right;" class="icon-remove icon-white"/></div></div></div>';
-    $('#disambiguation_panel').append(piece);
-    $('#close-disambiguate-panel').bind('click', function () {
-        $('#disambiguation_panel').hide();
-    });
-
-    $('#disambiguation_panel').show();
-};
-
-var getPieceShowexpandNERD = function (jsonObject){
-
-    var piece = '<div class="well col-md-12" style="background-color:#F7EDDC;">';
+    var piece = '<div class="well col-md-11" style="background-color:#F7EDDC;">';
     if (jsonObject['entities']) {
         piece += '<table class="table" style="border:1px solid white;">';
         for (var sens in jsonObject['entities']) {
@@ -78,8 +74,7 @@ var getPieceShowexpandNERD = function (jsonObject){
                 colorLabel = type;
             else if (domains && domains.length > 0) {
                 colorLabel = domain;
-            }
-            else
+            } else
                 colorLabel = entity.rawName;
 
             var start = parseInt(entity.offsetStart, 10);
@@ -102,22 +97,21 @@ var getPieceShowexpandNERD = function (jsonObject){
                     sens + '" href="' + wikipedia + '" rel="$teiCorpus.$standoff.$nerd.wikipediaExternalRef">';
             piece += '<input type="checkbox" id="selectEntity' + sens
                     + '" name="selectEntity' + sens + '" value="0" href="'
-                    + preferredTerm + '" rel="$teiCorpus.$standoff.$nerd.preferredTerm">';
+                    + preferredTerm + '" rel="$teiCorpus.$standoff.$nerd.preferredTerm" display="concepts">';
             piece += '<label for="selectEntity' + sens + '" id="label' + sens + '"> <strong>' + entity.rawName + '&nbsp;</strong> </label></div></td>';
             //piece += '<td><strong>' + entity.rawName + '&nbsp;</strong></td><td>'+ 
             if (preferredTerm) {
                 piece += '<td><b>' + preferredTerm + ': <b>' +
                         definitions[0]['definition']
                         + '</td><td>';
-            }
-            else {
+            } else {
                 piece += '<td>' +
                         definitions[0]['definition']
                         + '</td><td>';
             }
 
             if (freebase != null) {
-                var urlImage = 'https://usercontent.googleapis.com/freebase/v1/image' + freebase+'?';
+                var urlImage = 'https://usercontent.googleapis.com/freebase/v1/image' + freebase + '?';
                 urlImage += 'maxheight=250';
                 urlImage += '&';
                 urlImage += 'key=' + options.api_key;
@@ -151,7 +145,9 @@ var getPieceShowexpandNERD = function (jsonObject){
     /*for (var surf in jsonObject['paraphrases']) {
      piece += '<p>' + jsonObject['paraphrases'][surf] + '</p>';
      }*/
-piece += '</div></div>';
+
+    piece += '</div>';
+    piece += '<div class="col-md-1"><a id="close-disambiguate-panel" onclick=\'$("#disambiguation_panel").hide()\'><span class="glyphicon glyphicon-remove"></span></a></div>';
 //    piece += '</div><div><div id="close-disambiguate-panel" \
 //					  style="position:relative;float:right;" class="glyphicon glyphicon-remove icon-white"/></div></div></div>';
     return piece;
@@ -160,7 +156,7 @@ piece += '</div></div>';
 var activateDisambButton = function (num) {
     //console.log($('#facetview_freetext').val());
     //if ($('#facetview_freetext').val()) {
-        $('#disambiguate'+num).attr("disabled", false);
+    $('#disambiguate' + num).attr("disabled", false);
 //    }
 //    else {
 //        $('#disambiguate').attr("disabled", true);
@@ -169,5 +165,5 @@ var activateDisambButton = function (num) {
 
 var deactivateDisambButton = function (num) {
 
-        $('#disambiguate'+num).attr("disabled", true);
+    $('#disambiguate' + num).attr("disabled", true);
 };
